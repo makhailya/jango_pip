f539ef4f1a42e1fefbd365cd930ca00d6eb70b5c
makhailya <makhailya@gmail.com>
2026-01-15 01:58:08 +0500

    fix

--- internet_shop/config/urls.py
+++ internet_shop/config/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Админ-панель Django
     path('admin/', admin.site.urls),
-    
+
     # Подключаем URLs приложения catalog
-    # Все адреса из catalog/urls.py будут доступны по корневому пути
     path('', include('catalog.urls')),
 ]
+
+# Добавляем возможность отображать медиафайлы в режиме DEBUG
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
